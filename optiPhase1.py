a231cee1a6f188f1f4feb18e88de6325ef8c65cd
Lauriane-git <lauriane.teyssier@student-cs.fr>
2022-02-16 16:09:35 +0100

    optiPhase3

--- optiPhase1.py
+++ optiPhase1.py
@@ -1,14 +1,10 @@
 # importations de module
-from msilib.schema import Binary
-from matplotlib.quiver import QuiverKey
 import numpy as np
-import matplotlib.pyplot as plt
 from gurobipy import *
-from pandas import array
-from sympy import quadratic_congruence
+
 
 # variables dont on hérite des programmes précédents
-# cni
+# C=capacité de l'ouvrier n à faire la tache i
 # nbre_employe numéroté de 0 à n-1
 # nbre_taches
 # D : tableau contenant la distance entre les tâches i et j en position (i,j)
@@ -20,22 +16,31 @@ def optimisation_1(C, nbre_employe, nbre_taches, D, Duree, Debut, Fin):
     # ajout variables de décisions
     # temps à laquelle commencent les tâches
     M = 1440  # majorant des temps
-    H = m.addMVar(shape=nbre_employe, lb=0)
+    H = m.addMVar(shape=nbre_taches, lb=0, ub=1440)
     Y = m.addMVar(shape=(nbre_employe, nbre_taches, nbre_taches), lb=0)
     X = m.addMVar(shape=(nbre_employe, nbre_taches,
-                         nbre_taches), lb=0, ub=1)  # vtype=GRB.INTEGER)
-
+                         nbre_taches),  vtype=GRB.BINARY)
     # modification des types des variables d'entrées pour s'assurer qu'elles conviennent
     C = np.array(C)
     D = np.array(D)
 
     # -- Ajout des constraintes --
+    # une même personne ne peut faire deux fois la même tache :
+    for n in range(nbre_employe):
+        for i in range(nbre_taches):
+            m.addConstr(X[n, i, i] == 0)
     # toute tâche doit avoir un départ et une arrivée
     for i in range(nbre_taches):
+        # chaque tache a bien été faite
         m.addConstr(sum(X[n, i, j] for n in range(nbre_employe)
-                        for j in range(nbre_taches)) >= 2)
+                        for j in range(nbre_taches)) == 2)
+        # chaque tâche doit bien être faite par la même personne
+        for n in range(nbre_employe):
+            m.addConstr(sum(X[n, i, j] for n in range(nbre_employe)
+                            for j in range(nbre_taches)) == 2)
+
     for i in range(nbre_taches):
-        for j in range(nbre_taches):
+        for j in range(i):
             for n in range(nbre_employe):
                 # l'employé doit être capable d'effectuer les tâches
                 m.addConstr(X[n, i, j] <= C[n, i])
