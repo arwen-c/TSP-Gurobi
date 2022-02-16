83b756168180d99ca609bd6f524472ef0d70126c
Cyprien Ecomard <cyprien.ecomard@tudent-cs.fr>
2022-02-16 16:50:50 +0100

    MAJ

--- opti_test.py
+++ opti_test.py
@@ -20,7 +20,7 @@ def optimisation_1(C, nbre_employe, nbre_taches, D, Duree, Debut, Fin):
     # ajout variables de décisions
     # temps à laquelle commencent les tâches
     M = 1440  # majorant des temps
-    H = m.addMVar(shape=nbre_taches, lb=0)
+    H = m.addMVar(shape=(nbre_employe, nbre_taches), lb=0)
     Y = m.addMVar(shape=(nbre_employe, nbre_taches, nbre_taches), lb=0)
     # X = m.addMVar(shape=(nbre_employe, nbre_taches,
     #                      nbre_taches), vtype=GRB.CONTINUOUS, lb=0, ub=1)
@@ -42,6 +42,13 @@ def optimisation_1(C, nbre_employe, nbre_taches, D, Duree, Debut, Fin):
     # for n in range(nbre_employe):
     #     m.addConstr(sum(X[n, i, j] for j in range(nbre_taches)) == 2)
 
+    # Essai de contrainte pour avoir une seule personne qui fait une tâche donnée
+    for i in range(nbre_taches):
+        for j in range(nbre_taches):
+            for n in range(nbre_employe):
+                m.addConstr(H[n, i] <= M*X[n, i, j])
+                m.addConstr(X[n, i, j] <= H[n, i])
+
     for i in range(nbre_taches):
         for j in range(nbre_taches):
             for n in range(nbre_employe):
@@ -49,14 +56,14 @@ def optimisation_1(C, nbre_employe, nbre_taches, D, Duree, Debut, Fin):
                 m.addConstr(X[n, i, j] <= C[n, i])
                 m.addConstr(X[n, i, j] <= C[n, j])
                 # la tache sera bien faite dans l'intervalle de temps choisit
-                m.addConstr(H[j]+Duree[j] <= Fin[j])
-                m.addConstr(H[j] >= Debut[j])
+                m.addConstr(H[n, j]+Duree[j] <= Fin[j])
+                m.addConstr(H[n, j] >= Debut[j])
                 # la personne n a le temps de faire la tache j à la suite de la tache i
                 m.addConstr(Y[n, i, j]+X[n, i, j] *
-                            (Duree[i]+D[i, j]/0.83333) <= H[j])  # 0.833 = vitesse des ouvriers en km.min-1 (équivaut à 50km.h-1)
-                m.addConstr(Y[n, i, j] <= H[i])
+                            (Duree[i]+D[i, j]/0.83333) <= H[n, j])  # 0.833 = vitesse des ouvriers en km.min-1 (équivaut à 50km.h-1)
+                m.addConstr(Y[n, i, j] <= H[n, i])
                 m.addConstr(Y[n, i, j] <= X[n, i, j]*M)
-                m.addConstr(Y[n, i, j] >= H[i]-M*(1-X[n, i, j]))
+                m.addConstr(Y[n, i, j] >= H[n, i]-M*(1-X[n, i, j]))
     for i in range(nbre_taches):
         for n in range(nbre_employe):
             m.addConstr(X[n, i, i] == 0)
