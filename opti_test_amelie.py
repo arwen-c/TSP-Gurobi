ecd7b5736ec9775d06d38bfbe94895426b0d6153
Lauriane-git <lauriane.teyssier@student-cs.fr>
2022-02-23 17:40:59 +0100

    a

--- opti_test_amelie.py
+++ opti_test_amelie.py
@@ -1,16 +1,10 @@
-# importations de module
+# Importations de module
 import numpy as np
 from gurobipy import *
 
 
-# variables dont on hérite des programmes précédents
-# C=capacité de l'ouvrier n à faire la tache i
-# nbre_employe numéroté de 0 à n-1
-# nbre_taches
-# D : tableau contenant la distance entre les tâches i et j en position (i,j)
-
-# Modification des données pour insérer le dépot
 def ajout_domicile(TasksDico, EmployeesDico):
+    """Modification des données pour insérer des tâches factices de départ et de retour au dépot ou domicile."""
     TasksEnhanced = TasksDico.copy()
     for row in EmployeesDico:
         # ajout d'une tâche au départ du domicile
@@ -22,47 +16,53 @@ def ajout_domicile(TasksDico, EmployeesDico):
                               'TaskDuration': 0, 'Skill': row['Skill'], 'Level': 0, 'OpeningTime': row['WorkingStartTime'], 'ClosingTime': row['WorkingEndTime']})
     return(TasksEnhanced)
 
-# Calculs à faire en fonction des tâches et des employes dico
-
 
 def optimisation_1(C, nbre_employe, nbre_taches, D, Duree, Debut, Fin):
+    """Variables dont on hérite des programmes précédents :
+    C = matrice des capacité de l'ouvrier n à faire la tache i ;
+    D = matrice contenant la distance entre les tâches i et j en position (i,j) ;
+    Duree = liste des durées des tâches ;
+    Debut = liste des horaires de début des tâches ;
+    Fin = liste des horaires de fin des tâches."""
 
+    # -- Création du modèle --
     m = Model("Modele exact simple")
-    # -- ajout variables de décisions --
+
+    # -- Ajout variables de décisions --
     M = 1440  # majorant des temps
     H = m.addMVar(shape=nbre_taches+2*nbre_employe, lb=0, ub=M)
-    # Y = m.addMVar(shape=(nbre_employe, nbre_taches+2 *
-    #                      nbre_employe, nbre_taches+2*nbre_employe), lb=0)
     X = m.addMVar(shape=(nbre_employe, nbre_taches+2*nbre_employe,
                          nbre_taches+2*nbre_employe),  vtype=GRB.BINARY)
-    # modification des types des variables d'entrées pour s'assurer qu'elles conviennent
+
+    # -- Modification des types des variables d'entrées pour s'assurer qu'elles conviennent --
     C = np.array(C)
     D = np.array(D)
 
-    #
-    # taille du tableau modifié = nombre de tâches réelles + nombre de tâches fictives
+    # -- Taille du tableau modifié = nombre de tâches réelles + nombre de tâches fictives --
     t = len(Debut)
+
     # -- Ajout des constraintes --
 
-    # chaque trajet a bien été fait une seule fois
+    # Chaque trajet a bien été fait une seule fois
     for i in range(t):
         m.addConstr(sum(X[n, i, j] for n in range(nbre_employe)
-                        for j in range(t)) == 1)  # fait sur les lignes. Devrait être fait sur les colonnes ?
-    
-    # Toute tâche à un départ et une arrivée faites par la même personne - INCOMPATIBLE BORDS
+                        for j in range(t)) == 1)
+
+    # Toute tâche a un départ et une arrivée faite par la même personne, cette condition n'est pas appliquée au départ et à l'arrivée
     for n in range(nbre_employe):
         for j in range(nbre_taches):
             m.addConstr(sum(X[n, j, k] for k in range(t)) == sum(
                 X[n, i, j] for i in range(t)))
-    
-    # effets de bord
+
+    # Effets de bord
     for n in range(nbre_employe):
         m.addConstr(sum(X[n, nbre_taches+n, j]
-                        for j in range(nbre_taches)) == 1)  # depart du depot
+                        for j in range(nbre_taches)) == 1)  # départ du dépôt
         m.addConstr(sum(X[n, i, nbre_taches+nbre_employe+n]
-                        for i in range(nbre_taches)) == 1)  # arrivee au depot
+                        for i in range(nbre_taches)) == 1)  # arrivée au dépôt
 
-    for i in range(t):  # Taches réelles + taches fictives
+    # Contraintes incluants une somme
+    for i in range(t):  # Tâches réelles + Tâches fictives
         for j in range(t):
             for n in range(nbre_employe):
                 # l'employé doit être capable d'effectuer les 2 tâches
@@ -78,15 +78,11 @@ def optimisation_1(C, nbre_employe, nbre_taches, D, Duree, Debut, Fin):
                 m.addConstr(H[j] >= Debut[j])
                 # la personne n a le temps de faire la tache j à la suite de la tache i
                 m.addConstr(X[n, i, j] * (H[i]+Duree[i]+D[i, j]/0.83333)
-                            <= H[j])  # 
+                            <= H[j])
                 # 0.833 = vitesse des ouvriers en km.min-1 (équivaut à 50km.h-1)
-                # m.addConstr(Y[n, i, j]+X[n, i, j] *
-                #             (Duree[i]+D[i, j]/0.83333) <= H[j])
-                # m.addConstr(Y[n, i, j] <= H[i])
-                # m.addConstr(Y[n, i, j] <= X[n, i, j]*M)
-                # m.addConstr(Y[n, i, j] >= H[i]-M*(1-X[n, i, j]))
 
-    # -- Ajout de la fonction objectif. Produit terme à terme
+    # -- Ajout de la fonction objectif.
+    # Produit terme à terme
     m.setObjective(sum(X[n, i, j]*D[i, j] for n in range(nbre_employe)
                        for i in range(t) for j in range(t)), GRB.MINIMIZE)
 
@@ -94,8 +90,4 @@ def optimisation_1(C, nbre_employe, nbre_taches, D, Duree, Debut, Fin):
     m.optimize()  # Résolution
 
     # -- Affichage des solutions --
-    print("Les solutions optimales sont")
-    print("X =", X.x)
-    print("H =", H.x)
-    print("avec pour valeur de l'objectif z =", m.objVal)
     return X.x, H.x, m.objVal
