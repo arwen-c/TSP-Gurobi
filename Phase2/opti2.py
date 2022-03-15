f665c7cdc9ea4560ee8f6e0a517b71dff68cdbf3
unknown <pierreanne.chanteau@student-cs.fr>
2022-03-15 21:42:51 +0100

    correction erreur de signe 3

--- Phase2/opti2.py
+++ Phase2/opti2.py
@@ -51,7 +51,7 @@ def optimisation2(C, nbre_employe, nbre_taches, nbreIndispoEmploye, D, Duree, De
                          nbre_taches+2*nbre_employe+nbreIndispoEmploye),  vtype=GRB.BINARY)
     L = m.addMVar(shape=(nbre_employe, nbre_taches,
                          nbre_taches), vtype=GRB.BINARY)
-    delta = m.addMVar(shape=(nbre_taches+2*nbre_employe+nbreIndispoEmploye,5), vtype=GRB.BINARY)
+    delta = m.addMVar(shape=(nbre_taches+2*nbre_employe+nbreIndispoEmploye,5,3), vtype=GRB.BINARY)
 
     # -- Modification des types des variables d'entrées pour s'assurer qu'elles conviennent --
     C = np.array(C)
@@ -119,19 +119,19 @@ def optimisation2(C, nbre_employe, nbre_taches, nbreIndispoEmploye, D, Duree, De
 
                 # - Effets temporels -
                 # la tache j sera bien faite dans l'intervalle de temps ou elle est ouverte
-                # nbreCreneauxJ=len(Fin[j])
+                nbreCreneauxJ=len(Fin[j])
 
-                # M = 60*24
+                for k in range(nbreCreneauxJ):
+                    # M(1-delta) <= x-x0 <= M.delta       x0<x SSI delta>1
+                    m.addConstr(-M*(1-delta[j,k,1]) <= H[j]-Debut[j][k])
+                    m.addConstr(H[j]-Debut[j][k] <= M*delta[j,k,1])
+                    # -M(1-delta) <= x1-x <= M.delta      x<x1 SSI delta>1
+                    m.addConstr(-M*(1-delta[j,k,2]) <= -H[j]+Fin[j][k]-Duree[j])
+                    m.addConstr(-H[j]+Fin[j][k]-Duree[j] <= M*delta[j,k,2])
+                    # il faut que les deux contraintes ci dessus soit vérifiées
+                    m.addConstr(delta[j,k,0] == delta[j,k,1]*delta[j,k,2])
 
-                # for k in range(nbreCreneauxJ):
-                #     # M(1-delta) <= x-x0 <= M.delta       x0<x SSI delta>1
-                #     m.addConstr(M*(1-delta[j,k]) <= H[j]-Debut[j][k])
-                #     m.addConstr(H[j]-Debut[j][k] <= M*delta[j,k])
-                #     # -M(1-delta) <= x1-x <= M.delta      x<x1 SSI delta>1
-                #     m.addConstr(-M*(1-delta[j,k]) <= -H[j]+Fin[j][k]-Duree[j])
-                #     m.addConstr(-H[j]+Fin[j][k]-Duree[j] <= M*delta[j,k])
-
-                # m.addConstr(sum(delta[j,k] for k in range(nbreCreneauxJ)) == 1)
+                m.addConstr(sum(delta[j,k,0] for k in range(nbreCreneauxJ)) == 1)
 
                 # la personne n a le temps de faire la tache j à la suite de la tache i et peut etre de faire sa pause déjeuner
                 if i < nbre_taches and j < nbre_taches:  # on est entre deux tâches réelles
