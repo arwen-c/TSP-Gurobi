16c814a734f56ba6d63aa964c36b46510f87c7dd
AmelieMEME <amelie.forgeotdarc@student-cs.fr>
2022-03-23 15:01:57 +0100

    contraintes de flot init et final maj

--- Phase2/opti2.py
+++ Phase2/opti2.py
@@ -82,10 +82,9 @@ def optimisation2(C, nbre_employe, nbre_taches, nbreIndispoEmploye, D, Duree, De
             m.addConstr(sum(X[n, j, k] for k in range(t)) == sum(
                 X[n, i, j] for i in range(t)))
 
-        # Les employés font bien leurs pauses (indisponibilités):
+    # Les employés font bien leurs pauses (indisponibilités):
     for n in range(nbre_employe):
         NomEmploye = EmployeesDico[n]['EmployeeName']
-        #print("nbreIndispoEmploye :{}".format(nbreIndispoEmploye))
         for i_unavail in range(nbreIndispoEmploye):
             # Il faut que ce soit le bon employé qui fasse la pause
             if TasksEnhanced[nbre_taches+2*nbre_employe+i_unavail]['TaskId'] == "Unavail" + NomEmploye:
@@ -93,17 +92,16 @@ def optimisation2(C, nbre_employe, nbre_taches, nbreIndispoEmploye, D, Duree, De
                                 for i in range(nbre_taches)) == 1)  # arrivé à la pause
                 m.addConstr(sum(X[n, nbre_taches+2*nbre_employe+i_unavail, i]
                                 for i in range(nbre_taches)) == 1)  # départ de la pause
-                #print("TasksEnhanced[nbre_taches+2*nbre_employe+i_unavail]['TaskId'] : {}".format(TasksEnhanced[nbre_taches+2 *nbre_employe+i_unavail]['TaskId']))
             else:  # Un autre ne peux pas piquer la pause d'un autre
                 m.addConstr(sum(X[n, i, nbre_taches+2*nbre_employe+i_unavail]
                                 for i in range(nbre_taches)) == 0)  # arrivé à la pause
 
-    # Effets de bord
+    # Contraintes de flot initiale et finale
     for n in range(nbre_employe):
         m.addConstr(sum(X[n, nbre_taches+n, j]
-                        for j in range(nbre_taches)) == 1)  # départ du dépôt
+                        for j in [k in range(nbre_taches)]+[k in range (nbre_taches+2*n,t)]) == 1)  # départ du dépôt
         m.addConstr(sum(X[n, i, nbre_taches+nbre_employe+n]
-                        for i in range(nbre_taches)) == 1)  # arrivée au dépôt
+                        for i in [k in range(nbre_taches)]+[k in range (nbre_taches+2*n,t)]) == 1)  # arrivée au dépôt
 
     # Ajout des contraintes sur L
     # Une pause dej n'est possible qu'entre deux taches réalisées
@@ -121,9 +119,6 @@ def optimisation2(C, nbre_employe, nbre_taches, nbreIndispoEmploye, D, Duree, De
                 # l'employé doit être capable d'effectuer les 2 tâches
                 m.addConstr(X[n, i, j] <= C[n, i])
                 m.addConstr(X[n, i, j] <= C[n, j])
-
-                # l'employé ne peut pas faire le trajet d'une tache vers elle-même : la diagonale doit être nulle
-                #m.addConstr(X[n, i, i] == 0)
 
                 # - Effets temporels -
                 # la tache j sera bien faite dans un intervalle de temps où elle est ouverte
