fbbff4bb2747704f0b70000f2836b4085b71ec01
Lauriane-git <lauriane.teyssier@student-cs.fr>
2022-03-23 16:17:27 +0100

    premiere version

---
+++ Phase3/usefulFunctions3.py
@@ -0,0 +1,19 @@
+
+
+def fonctionRestriction(capacite, X):
+    pass
+
+
+def triOpti(tachesFaisables, localisationCourante, distance):
+    pass
+
+
+def tachesRealisable(tacheOpti, duree, debut, fin, employe['EndTime'], indispoDico):
+    '''
+    fonction qui vérifie qu'au moins une tâche est réalisable par l'employé avant la fin de la journée, qui retourne :
+    - raison valant 'indisponibilité', si une indisponibilité bloque la réalisation d'une de ces tâches, 'fin de journée' si aucun tâche n'est faisable avant la fin de journée, 'déjeuner' si le déjeuner bloque, none sinon
+    - tache : le numéro de la tache optimale faisable, si une tache au moins est faisable, none sinon
+    '''
+    # heureButtoire = min(heure de fin, heure d'indispo - si existe-, Hfin de pause dej-temps de dej si pause non faite)
+
+    pass
